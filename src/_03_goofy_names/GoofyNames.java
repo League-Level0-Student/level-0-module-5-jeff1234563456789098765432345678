2e36e3f52d8d1e96504f67f768d46c9966b86fb9
League Student <league@iMac-9.attlocal.net>
2019-12-12 18:28:14 -0800

    GoofyNames,Guessing PI :IP: :smile:

--- src/_03_goofy_names/GoofyNames.java
+++ src/_03_goofy_names/GoofyNames.java
@@ -11,20 +11,26 @@ public class GoofyNames {
 		String goofyName = "";
 
 		// 1. Ask the user to enter their name
-
+			String a = JOptionPane.showInputDialog("Whats your name You name must be 6 letters");
 		// 2. Print upper case name to the console using .toUpperCase()
 		//    Run your program to see that this works.
+			System.out.println(a.toUpperCase());
 
 		// 3. Loop through each character of the name (steps 4 - 6).
 		//    HINT: Use .length() to determine the number of characters in the String.
-
+			a.length();
+		
 			// 4. Create a String variable to store the next character of the name
 			//    using .substring(start, end)
 			//    HINT: replace 'start' and 'end' to get String with character at i 
-			
-			// 5. Use MODULO to set this variable to upper case for EVEN characters
+		String b = null;
+			b.substring(0,6);
+					
 			//    and lower case for ODD characters.
-
+			int i = 0;
+			boolean a1=(i%2==1);{
+				 
+				  }
 			
 			// 6. ADD this String  (containing 1 char) to the goofyName String
 
--- src/_04_guessing_pi/GuessingPi.java
+++ src/_04_guessing_pi/GuessingPi.java
@@ -8,14 +8,18 @@ import javax.swing.JOptionPane;
 
 public class GuessingPi {
 	// 1. Make a main method and make sure your program can run
+public static void main(String[] args) {
+	
 
 		// 2. Make a String variable to hold the value of Pi.
 		//    Get the first few digits from http://www.piday.org/million/.
-
+			String pi = "3.141592653589793238462643383279502884197169399375105820";
 		// 3. Print out the first 3 digits of Pi to the console.
 		//    The first value is "pi.charAt(0)", the second is "pi.charAt(1)", etc.
 		//    Run your program to see how this works.
-
+			System.out.println( pi.charAt(0));
+			
+			
 		// 9. If you want to give the user more than one chance to guess,
 		//    put a for loop around steps 4-8.
 
@@ -36,4 +40,4 @@ public class GuessingPi {
 
 }
 
-
+}
