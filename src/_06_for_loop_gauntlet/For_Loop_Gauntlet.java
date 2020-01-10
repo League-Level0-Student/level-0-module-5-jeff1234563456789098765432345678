f4dbc6c8f2a220a87ced5732a316b2047c0c7bde
League Student <league@iMac-12.attlocal.net>
2020-01-09 18:29:20 -0800

    For_Loop_Gauntlet:IP:  :smile:

--- src/_06_for_loop_gauntlet/For_Loop_Gauntlet.java
+++ src/_06_for_loop_gauntlet/For_Loop_Gauntlet.java
@@ -2,4 +2,25 @@ package _06_for_loop_gauntlet;
 
 public class For_Loop_Gauntlet {
 
+	public static void main(String[] args) {
+//		for (int i = 0; i < 100; i++) {
+//			System.out.println(i+1);
+//		}
+//		
+//		for (int i = 101; i >0; i--) {
+//			System.out.println(i-1);
+//		}
+//		
+//		for (int i = 2; i < 101; i++) {
+//			if (i % 2 == 0) {
+//				System.out.println(i);
+//			}
+//		}
+		for (int j = 1; j < 100; j++) {
+			if (j % 3 == 0) {
+				System.out.println(j);
+			}
+		}
+	}
+
 }
