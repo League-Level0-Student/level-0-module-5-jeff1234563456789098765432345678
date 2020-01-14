5b7455c38e16377ebd68a3992f924c4b05767759
League Student <league@iMac-9.attlocal.net>
2020-01-13 19:56:34 -0800

    For_Loop_Gauntlet, Skill_Practice:IP:   :Smile:

--- src/_06_for_loop_gauntlet/For_Loop_Gauntlet.java
+++ src/_06_for_loop_gauntlet/For_Loop_Gauntlet.java
@@ -3,24 +3,72 @@ package _06_for_loop_gauntlet;
 public class For_Loop_Gauntlet {
 
 	public static void main(String[] args) {
-//		for (int i = 0; i < 100; i++) {
-//			System.out.println(i+1);
-//		}
-//		
-//		for (int i = 101; i >0; i--) {
-//			System.out.println(i-1);
-//		}
-//		
-//		for (int i = 2; i < 101; i++) {
-//			if (i % 2 == 0) {
-//				System.out.println(i);
-//			}
-//		}
+		/**for (int i = 0; i < 100; i++) {
+			System.out.println(i+1);
+		}
+		
+		for (int i = 101; i >0; i--) {
+			System.out.println(i-1);
+		}
+		
+		for (int i = 2; i < 101; i++) {
+			if (i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
 		for (int j = 1; j < 100; j++) {
 			if (j % 3 == 0) {
 				System.out.println(j);
 			}
-		}
-	}
-
+			for (int i = 1; i < 501; i++) {
+				if (i % 2 == 0) {
+					System.out.println(i +  "Even");
+				}
+				else {
+					System.out.println(i +  "Odd");
+				
+				}
+				for (int k = 0; k < 777; k++) {
+					if (i % 7 == 0) {
+						System.out.println(i);
+					}**/
+				//	for (int k2 = 2007; k2 < 2020; k2++) {
+					//	System.out.println("in "+ k2 + " I was " + (k2-2007));
+						
+				//	}
+			//	}
+//for (int i = -1; i < 2; i++) {
+//	System.out.println(i+1);
+//	for (int j = -1; j < 2; j++) {
+//		System.out.println(i+1);
+//	}
+//}}}
+/**	for (int i = 1; i < 4; i++) {
+		System.out.print(i);
+	}	
+	System.out.println();
+for(int i= 4; i < 7; i++) {
+	System.out.print(i);
 }
+System.out.println();
+for(int i= 7; i < 10; i++) {
+	System.out.print(i);
+}}}
+	**/	
+for (int i = 0; i < 100; i+=10) {
+	System.out.println(i);
+	for (int j = i+1; j < 11; j++) {
+		System.out.println(j);
+	}
+}
+
+}}
+
+
+
+
+//}
+		//}
+	//}
+
+//}
---
+++ src/_07_skill_practice/Skill_Practice.java
@@ -0,0 +1,13 @@
+package _07_skill_practice;
+
+import javax.swing.JOptionPane;
+
+public class Skill_Practice {
+public static void main(String[] args) {
+	
+String a = JOptionPane.showInputDialog("How many Dimes do you have?");
+	
+	
+	
+}
+}
